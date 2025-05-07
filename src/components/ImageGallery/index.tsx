a76b4e8c86921486e9247916e56cc1b824e0ee25
martinxt <martinxaviertchembe@gmail.com>
2025-05-07 15:52:11 +0100

    restarting ImageGallery from a fresh status

--- src/components/ImageGallery/index.tsx
+++ src/components/ImageGallery/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { ImageGalleryProps } from "./types";
 import SingleImage from "./SingleImage";
+import { Variants } from "framer-motion";
 
 const ImageGallery: React.FC<ImageGalleryProps> = (props: ImageGalleryProps) => {
     
+    const enterLeave: Variants = {
+        enter: {
+            opacity: 1
+        },
+        leave: {
+            opacity: 0
+        }
+    }
+
     return (
-        <div style={{width: props.containerWidth}}>
+        <div className="overflow-hidden" style={{width: props.containerWidth}}>
             {(props.images.length >= 1)?
                 <SingleImage 
                     imageUrl={props.images[0]} 
